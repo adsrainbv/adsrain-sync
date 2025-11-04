61611123c2144e9842da768e321af3d314b8f626
adsrainbv <adsrainbv@gmail.com>
2025-11-04 15:45:03 +0100

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,2 +1,3 @@
-export default (req, res) => res.redirect(302, '/api/auth/google-ads/start?tenantId=demo&returnTo=%2Fsettings');
-
+export default function handler(req, res) {
+  return res.redirect('/api/auth/google-ads/start');
+}
